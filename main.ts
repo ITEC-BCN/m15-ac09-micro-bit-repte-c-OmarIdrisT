8d5515bb5435a76bd5c35a8f012ed491eb3bf0d6
OmarIdrisT <146827333+OmarIdrisT@users.noreply.github.com>
2024-10-28 15:24:02 +0100

    Acabat

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
-	
+let dau = 0
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber > dau) {
+        basic.showIcon(IconNames.Sad)
+    } else if (receivedNumber < dau) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.Asleep)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    dau = randint(1, 6)
+    basic.showNumber(dau)
+    radio.sendNumber(dau)
 })
